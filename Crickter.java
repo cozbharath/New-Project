6638466c809d77b520b2221aad147b748d36ff36
Navabharath podila <Navabharath.podila@gmail.com>
2026-02-10 15:47:58 +0530

    first commit

---
+++ Crickter.java
@@ -0,0 +1,8 @@
+public class Crickter {
+
+	static public void main(String[] Bharath) {
+		System.out.println("Good Morning");
+		System.out.println("Welcome to VCube");
+		System.out.println("Java Full Stack");
+	}
+}
